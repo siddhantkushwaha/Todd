b70914b7d28631fc64ba27e17adb30b26cadf7af
Siddhant Kushwaha <k16.siddhant@gmail.com>
2020-05-16 01:52:08 +0530

    gradle task to build jar

---
+++ src/main/kotlin/com/siddhantkushwaha/todd/gdrive/GDriveApp.kt
@@ -0,0 +1,31 @@
+package com.siddhantkushwaha.todd.gdrive
+
+class GDriveApp {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            if (args.isEmpty())
+                throw Exception("Command not found.")
+
+            val gDrive = GDrive()
+            when (args[0]) {
+                "download" -> {
+                    if (args.size < 3)
+                        throw Exception("Less arguments than expected.")
+                    gDrive.downloadLocally(fileId = args[1], downloadDir = args[2], numWorkers = 8)
+                }
+
+                "upload" -> {
+                    if (args.size < 4)
+                        throw Exception("Less arguments than expected.")
+                    gDrive.upload(filePath = args[1], fileType = args[2], parentId = args[3])
+                }
+            }
+        }
+    }
+}
+
+
+
